9b824d7101bf8bed7c2f8855f998aed3c3344927
eric <eric@erics-MacBook-Pro.local>
2019-10-30 16:01:02 -0400

    .

--- src/git-source-provider.ts
+++ src/git-source-provider.ts
@@ -103,19 +103,17 @@ export async function getSource(
     }
 
     // Remove possible previous extraheader
+    // todo: should this be http.https://github.com/.extraheader instead ?
     let extraHeaderConfigKey = `http.${repositoryUrl}/.extraheader`;
     await removeGitConfig(git, extraHeaderConfigKey);
 
     // Add extraheader (auth)
-console.log(`************ AUTH = x-access-token:${accessToken}`);
     let base64Credentials = Buffer.from(`x-access-token:${accessToken}`, 'utf8').toString('base64');
     core.setSecret(base64Credentials);
+    await git.config('http.https://github.com/.extraheader', `AUTHORIZATION: basic asdf`);
     await git.config(extraHeaderConfigKey, `AUTHORIZATION: basic ${base64Credentials}`);
+    // await git.config('http.https://github.com/.extraheader', `AUTHORIZATION: basic asdf`);
 
-    // ?
-    // extraHeaderConfigKey = `http.https://github.com/.extraheader`;
-    // await removeGitConfig(git, extraHeaderConfigKey);
-    // await git.config(extraHeaderConfigKey, `AUTHORIZATION: basic ${base64Credentials}`);
 
     // LFS install
     if (lfs) {
